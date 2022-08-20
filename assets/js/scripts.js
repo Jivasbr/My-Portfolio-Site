79e7c376c1a488ffdd359c217ba913ac087683c9
Jivasbr <jivago.januario@outlook.com.br>
2022-08-20 02:17:01 -0300

    adjusts and fix for menu indicator on desktop version

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -301,11 +301,17 @@ function boxOpen(x, y) {
 // Slide Menu Indicator
 
 var marker = document.querySelector('#marker');
-var item = document.querySelectorAll('.menu a');
+var item = document.querySelectorAll('.menuIndicator a');
 
 function indicator(e){
 marker.style.left = e.offsetLeft + 'px';
 marker.style.width = e.offsetWidth + 'px';
-
 }
 
+item.forEach(Link => {
+Link.addEventListener('click', (e)=>{
+
+indicator(e.target);
+})
+
+})
